d03bc781f2dc92dacc0336964a2ff03b284deac0
Paulo Rodriguez <paulo_crr@hotmail.com>
2023-03-02 22:53:25 -0500

    feat: Se agergo cache a las imagenes de la pagina de detalle

--- lib/pages/character_details_page.dart
+++ lib/pages/character_details_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:marvel_characters_app/models/character_details.dart';
 import 'package:marvel_characters_app/services/characters_service.dart';
@@ -47,7 +48,12 @@ class _DetailsInformation extends StatelessWidget {
       children: [
         Hero(
           tag: details.id,
-          child: Image.network(details.thumbnail ?? ""),
+          child: CachedNetworkImage(
+            imageUrl: details.thumbnail ?? "",
+            progressIndicatorBuilder: (_, __, downloadProgress) =>
+                LinearProgressIndicator(value: downloadProgress.progress),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
         ),
         const SizedBox(height: 8),
         Text(
